c9fae332b1be2161c509d2f20a6b13e7bb1ca7b8
Jason Kingsmill <jason.kingsmill@outlook.com>
2019-06-28 08:53:39 +1000

    Add data layer project with base repository implementations

---
+++ src/Data/OrderRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Data
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private List<Order> _orders;
+
+
+        public OrderRepository(List<Order> orders)
+        {
+            _orders = orders;
+        }
+
+        Order IOrderRepository.GetById(int id)
+        {
+            return _orders.FirstOrDefault(o => o.OrderId == id);
+        }
+
+
+    }
+}
---
+++ src/Data/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        private List<Product> _products;
+
+
+        public ProductRepository(List<Product> products)
+        {
+            _products = products;
+        }
+
+        public Product GetById(int id)
+        {
+            return _products.FirstOrDefault(p => p.ProductId == id);
+        }
+
+
+    }
+}
