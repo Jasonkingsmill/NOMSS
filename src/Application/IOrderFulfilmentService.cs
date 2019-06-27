8907bb759007d39ace4aa8ca076ed809b5c6fcad
Jason Kingsmill <jason.kingsmill@outlook.com>
2019-06-28 08:55:59 +1000

    Add Application layer project and OrderFulfilmentService

---
+++ src/Application/IOrderFulfilmentService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application
+{
+    public interface IOrderFulfilmentService
+    {
+        IEnumerable<int> FulfilOrders(List<int> orderIds);
+    }
+}
---
+++ src/Application/OrderFulfilmentService.cs
@@ -0,0 +1,37 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application
+{
+    public class OrderFulfilmentService : IOrderFulfilmentService
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly OrderProcessingService _orderProcessingService;
+
+        public OrderFulfilmentService(IOrderRepository orderRepository, OrderProcessingService orderProcessingService)
+        {
+            _orderRepository = orderRepository;
+            _orderProcessingService = orderProcessingService;
+        }
+
+        public IEnumerable<int> FulfilOrders(List<int> orderIds)
+        {
+            List<int> unfulfilledOrders = new List<int>();
+            foreach (var orderId in orderIds)
+            {
+                var order = _orderRepository.GetById(orderId);
+                try
+                {
+                    _orderProcessingService.ProcessOrder(order);
+                }
+                catch
+                {
+                    unfulfilledOrders.Add(orderId);
+                }
+            }
+            return unfulfilledOrders;
+        }
+    }
+}
