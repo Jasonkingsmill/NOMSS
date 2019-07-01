e470e900de26f48299fd175677a263956834d6ac
Jason Kingsmill <jason.kingsmill@outlook.com>
2019-07-01 14:40:04 +1000

    Add version to controller

--- src/Web/Controllers/PurchaseOrdersController.cs
+++ src/Web/Controllers/PurchaseOrdersController.cs
@@ -9,7 +9,7 @@ using Web.Models;
 
 namespace Web.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class PurchaseOrdersController : ControllerBase
     {
