a76d7e046c8c13533778bf95029019323d721c99
Jason Kingsmill <jason.kingsmill@outlook.com>
2019-07-01 14:42:45 +1000

    Add tests

--- tests/UnitTests/OrderProcessingService_Tests.cs
+++ tests/UnitTests/OrderProcessingService_Tests.cs
@@ -9,14 +9,14 @@ namespace UnitTests
 {
     public class OrderProcessingService_Tests
     {
-        public class Fulfill : OrderProcessingService_Tests
+        public class ProcessOrder : OrderProcessingService_Tests
         {
             private Mock<IProductRepository> _productRepository;
             private Mock<IPurchaseOrderRepository> _purchaseOrderRepository;
             private Mock<IDomainEventDispatcher> _domainEventDispatcher;
             private OrderProcessingService _orderProcessingService;
 
-            public Fulfill()
+            public ProcessOrder()
             {
                 _domainEventDispatcher = new Mock<IDomainEventDispatcher>();
             }
@@ -56,6 +56,45 @@ namespace UnitTests
                 // Assert
                 Assert.Equal(OrderStatus.Fulfilled, order.Status);
                 Assert.Equal(quantityOnHand - orderQuantity, product.QuantityOnHand);
+            }
+
+
+
+
+            [Fact]
+            public void Pending_order_should_not_be_processed_when_product_included_has_no_stock_available()
+            {
+                // Arrange
+                var orderId = 9987;
+                int productId = 1234;
+                int quantityOnHand = 4;
+                int orderQuantity = 5;
+                decimal costPerItem = 10.00m;
+                int reorderThreshold = 5;
+                int reorderAmount = 5;
+
+                var orderItem = new OrderItem(productId, orderQuantity, costPerItem);
+                var order = new Order(orderId, DateTime.Now, OrderStatus.Pending, new List<OrderItem>()
+                {
+                    orderItem
+                });
+
+                var product = new Product(productId, "test product", quantityOnHand, reorderThreshold, reorderAmount, 10);
+
+                _productRepository = new Mock<IProductRepository>();
+                _productRepository.Setup(x => x.GetById(It.Is<int>(p => p == productId))).Returns(product);
+                _purchaseOrderRepository = new Mock<IPurchaseOrderRepository>();
+                _purchaseOrderRepository.Setup(x => x.Add(It.IsAny<PurchaseOrder>()));
+
+                _orderProcessingService = new OrderProcessingService(_productRepository.Object, _purchaseOrderRepository.Object, _domainEventDispatcher.Object);
+
+
+                // Act
+                _orderProcessingService.ProcessOrder(order);
+
+
+                // Assert
+                Assert.Equal(OrderStatus.Error_Unfulfillable, order.Status);
             }
 
 
